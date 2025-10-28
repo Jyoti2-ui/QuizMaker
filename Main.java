621bcff05168813e8163d3aacdf4aa413c6ebb7f
neha-rajpal <neharajpal245@gmail.com>
2025-10-28 19:25:40 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 package src.main;
 
+import javax.swing.SwingUtilities;
+import src.ui.QuizCreationUI;
+
+/**
+ * Entry point of the Quiz Maker application.
+ * This class initializes and launches the main UI using Swing.
+ */
 public class Main {
-    
+    public static void main(String[] args) {
+        // Run UI on Event Dispatch Thread for thread safety
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new QuizCreationUI().setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
